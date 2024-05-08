dc3d326a7c03a2994f5d5b36d7fcbbc613595a93
JP <janphsantiago@gmail.com>
2024-05-08 21:11:31 +0800

    Added buttons for eraser and pen options
    Updated the HTML, style and JS with eraser and pen.
    Created an event listener for buttons and conditional loop
    for different modes for pen, eraser and the default "random" color.

--- etch.js
+++ etch.js
@@ -1,15 +1,20 @@
 const generateGridBtn = document.getElementById("generateGridBtn");
+const eraserBtn = document.getElementById("eraser");
+const penBtn = document.getElementById("pen");
+const colorBtn = document.getElementById("color");
 
+let mode = "color";
 
 generateGridBtn.addEventListener("click", function() {
-    
+
+
     // Clear existing grid
     const gridContainer = document.getElementById("gridContainer");
     gridContainer.innerHTML = "";
     
-    // Prompt the user to enter the number of grid squares
     let rowSquares = parseInt(prompt("Enter the number of grid squares (1 - 100):"));
-
+   
+    // Loop to enter a valid number 1- 100
     while (rowSquares < 0 || rowSquares > 100) {
          rowSquares =  parseInt(prompt("ERROR! Please enter a valid number"));
          }
@@ -27,12 +32,30 @@ generateGridBtn.addEventListener("click", function() {
         gridItem.addEventListener("mouseover", function() {
             let letters = '0123456789ABCDEF';
             let color = '#';
+            if (mode == "color"){
             for (let i = 0; i < 6; i++) {
                 color += letters[Math.floor(Math.random() * 16)];
               }
             gridItem.style.backgroundColor = color;
+            }
+            else if (mode == "eraser"){
+                gridItem.style.backgroundColor = "";
+            }
+            else if (mode == "pen"){
+                gridItem.style.backgroundColor = "black";
+            }
         });
 
         gridContainer.appendChild(gridItem);
     }
+
+    colorBtn.addEventListener("click", function () {
+        mode = "color";
+    });
+    eraserBtn.addEventListener("click", function () {
+        mode = "eraser";
+    });
+    penBtn.addEventListener("click", function () {
+        mode = "pen";
+    });
 });
